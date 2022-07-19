a7c36544f9688fdffbabf3006ef83bbaec9193f9
shubhamidsharma <shubhamidsharma@gmail.com>
2022-07-19 15:08:22 +0530

    starting with mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
-const express = require('express') 
-const products = require('./data/products')
-const app = express()
+import express from "express";
+import dotenv, { config } from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
-app.get('/',(req,res)=>{
-  res.send('API is running...')
-})
- 
-app.get('/api/products',(req,res)=>{
-  res.json(products)
-})
 
-app.get('/api/products/:id',(req,res)=>{
-  const product=products.find(p=>p._id===req.params.id)
-  res.json(product)
-})
+const app = express();
+config();
+connectDB();
 
-app.listen(5000,console.log('Server running on Port 5000'))
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+console.log(PORT);
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+);
