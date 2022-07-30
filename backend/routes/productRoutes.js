f45ae7a2062b0adcbc0cd72db03e91f0c487cb68
shubhamidsharma <shubhamidsharma@gmail.com>
2022-07-30 12:50:31 -0700

    backend for user authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,12 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler';
 const router = express.Router()
-import Product from '../models/productModel.js'
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
 
-// Fetch All products
-// GET /api/products
-// Public
+router.route('/').get(getProducts)
 
-router.get("/", asyncHandler(async(req, res)=> {
-  const products = await Product.find();
-  //res.status(401)
-  //throw new Error('Not Authorized') 
-  res.json(products); 
-}));
-
-// Fetch All products
-// GET /api/products
-// Public
-
-router.get("/:id", asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id);
-  if(product)
-    res.json(product);
-  else
-    res.status(404)
-    throw new Error('Product not Found')
-}));
+router.route('/:id').get(getProductById)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,35 @@
-import express from "express";
-import dotenv, { config } from "dotenv";
-import connectDB from "./config/db.js";
+import express from 'express'
+import { config } from 'dotenv'
+import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
 
-const app = express();
-config();
-connectDB();
+const app = express()
 
+config()
 
+connectDB()
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(express.json())
 
-app.use('/api/products',productRoutes)
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
-console.log(PORT);
+const PORT = process.env.PORT || 5000
+console.log(PORT)
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold)
-);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+  )
+)
