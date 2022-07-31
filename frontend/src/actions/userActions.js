07d4faf63bf257b98a4fe10e7e52d124fdcb416a
shubhamidsharma <shubhamidsharma@gmail.com>
2022-07-31 17:14:35 +0530

    Fixed Errors of profile update and more

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -153,13 +153,22 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
     })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not auth, Token failed') {
+      dispatch(logout())
+    }
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
+      payload: message,
     })
   }
 }
